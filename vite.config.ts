8d52fa67d6f6f69fb444c10be2c3b31610d0b84e
PaSchunk <paschunk@visualevents.info>
2025-12-18 17:36:36 +0100

    change to /

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-const repoBase = '/Chillout-Turnierseite/'
+const defaultBase = '/'
 
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH ?? repoBase,
+  base: process.env.VITE_BASE_PATH ?? defaultBase,
   plugins: [react() as any],
   test: {
     globals: true,
